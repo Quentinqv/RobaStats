aa2ac9ddfb1494eaac93f079d6c704250c27664c
Vitoux Quentin <quentin010801@gmail.com>
2022-08-24 10:59:22 +0200

    📦 NEW: PWA

--- main_complexe.php
+++ main_complexe.php
@@ -211,6 +211,7 @@ foreach ($parsedRows as $key => $value) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="manifest" href="/manifest.webmanifest">
   <title>RobaStats</title>
 </head>
 <style>
@@ -272,6 +273,19 @@ foreach ($parsedRows as $key => $value) {
   <?php
   }
   ?>
+  <script>
+    if ('serviceWorker' in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('Service worker registration succeeded:', registration);
+      }, /*catch*/ function(error) {
+        console.log('Service worker registration failed:', error);
+      });
+    } else {
+      console.log('Service workers are not supported.');
+    }
+  </script>
 
 </body>
 <!-- Copyrights VITOUX Quentin Jan. 2022 -->
